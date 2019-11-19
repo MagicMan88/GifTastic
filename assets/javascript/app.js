c90d8f85b75df8495a8590e8f4eb48fe5db02bde
Stephen Green <stephengreen1028@gmail.com>
2019-11-18 19:11:31 -0700

    adjusting app.js and index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,45 @@
 // Variables
 // ============================================
 
-// URL for giphy API
-var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9"
+// Event listener for all button elements
+$("button").on("click", function() {
+    // In this case, the "this" keyword refers to the button that was clicked
+    var person = $(this).attr("data-person");
+    // Constructing a URL to search Giphy for the name of the person who said the quote
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=EDIhLQCerntt3WVSp8CaSSY2I70Ug4Di&limit=5";
 
+    // Actually getting the data from the API (Performing our AJAX GET method)
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      // After the data comes back from the API
+      .then(function(response) {
+        // Store an array of results in the results variable
+        var results = response.data;
 
+        for (var i = 0; i < results.length; i++) {
+          var gifDiv = $("<div>");
+
+          var rating = results[i].rating;
+
+          var p = $("<p>").text("Rating: " + rating);
+
+          var personImage = $("<img>");
+          personImage.attr("src", results[i].images.fixed_height.url);
+
+          gifDiv.prepend(p);
+          gifDiv.prepend(personImage);
+
+          $("#gifs-appear-here").prepend(gifDiv);
+        }
+      });
+  });
 
 // Functions
 // ============================================
 
+
+
 // Main Processes
 // ============================================
-
-// Calling in the API using an ajax call
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-  });
