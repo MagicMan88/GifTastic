9911491b6e456217905e061c2063460a3396a96f
Stephen Green <stephengreen1028@gmail.com>
2019-11-18 20:47:49 -0700

    adding new logic to app.js and changing a few id's in index.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,46 @@
-// Variables
-// ============================================
-
-// Event listener for all button elements
-$("button").on("click", function() {
-    // In this case, the "this" keyword refers to the button that was clicked
-    var person = $(this).attr("data-person");
-    // Constructing a URL to search Giphy for the name of the person who said the quote
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=EDIhLQCerntt3WVSp8CaSSY2I70Ug4Di&limit=5";
-
-    // Actually getting the data from the API (Performing our AJAX GET method)
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      // After the data comes back from the API
-      .then(function(response) {
-        // Store an array of results in the results variable
-        var results = response.data;
-
-        for (var i = 0; i < results.length; i++) {
-          var gifDiv = $("<div>");
-
-          var rating = results[i].rating;
-
-          var p = $("<p>").text("Rating: " + rating);
-
-          var personImage = $("<img>");
-          personImage.attr("src", results[i].images.fixed_height.url);
-
-          gifDiv.prepend(p);
-          gifDiv.prepend(personImage);
-
-          $("#gifs-appear-here").prepend(gifDiv);
-        }
-      });
-  });
-
-// Functions
-// ============================================
-
-
-
 // Main Processes
 // ============================================
+$(document).ready(function () {
+    // Array of topics, mine is favorite fictional characters
+    var topics = ["Harry Potter", "Gandalf", "James Bond", "Sirius Black", "Chicken Little"];
+    // Create a function to dynamically generate and display data
+
+    function renderButtons() {
+        // Delete buttons prior to adding new ones otherwise will have repeats
+        $("buttons").empty();
+        // Loop through the array of topics, dynamically creating buttons for each topic
+        for (var i = 0; i < topics.length; i++) {
+            // Create new div
+            var gifDiv = $('<div>');
+            var rating = results[i].rating;
+            // Create new p tag
+            var p = $('<p>').text("Rating " + rating);
+            // Create new img tag
+            var personImage = $('<img>');
+            personImage.attr("src", results[i].images.fixed_height.url);
+            // preprend to html
+            gifDiv.prepend(p);
+            gifDiv.prepend(personImage);
+            // Add gifs to 'gifs-go-here' id
+            $('gifs-go-here').prepend(gifDiv);
+        }
+    }
+
+    // Call renderButtons function
+    renderButtons();
+
+    // Add a new button for each new fictional character
+    $('#add-character').on('click', function () {
+        event.preventDefault();
+        // Assign character to character-input id
+        var character = $('#character-input').val().trim();
+        // Add new character to topics array
+        topics.push(character);
+        // Generate button for new character
+        renderButtons();
+        return;
+    });
+
+
+
+});
